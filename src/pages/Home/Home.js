94f6a6c1c8287ed517e0724b9cfc17acc2994eb4
Caio Augusto <caioamfr@gmail.com>
2020-11-10 16:09:50 -0300

    Componente Head adicionado

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import Head from "../../Components/Helper/Head/Head";
 import PalletBox from '../../Components/PalletBox/PalletBox'
 
 const Home = () => {
   document.title = 'Pick Pallet | Uma coleção de paletas';
   return (
     <div>
+      <Head title="Paletas" />
       <Header />
       <PalletBox />
     </div>
