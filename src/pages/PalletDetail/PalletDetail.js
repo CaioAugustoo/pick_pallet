5f2941117b8af25b1657916fab55a12bd07a0a35
Caio Augusto <caioamfr@gmail.com>
2020-11-10 16:10:41 -0300

    Componente Head adicionado

--- src/pages/PalletDetail/PalletDetail.js
+++ src/pages/PalletDetail/PalletDetail.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../Components/Header/Header';
+import Head from "../../Components/Helper/Head/Head";
 import PalletDetails from '../../Components/PalletDetails/PalletDetails';
 
 const PalletDetail = () => {
   document.title = 'Pick Pallet | Detalhes';
   return (
     <div>
+      <Head title="Detalhes" />
       <Header />
       <PalletDetails />
     </div>
