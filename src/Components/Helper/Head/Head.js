af249e0bace1867e1ea1b9a2d69a6f0bd35d08e3
Caio Augusto <caioamfr@gmail.com>
2020-11-10 16:11:03 -0300

    Componente Head

---
+++ src/Components/Helper/Head/Head.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+const Head = props => {
+  useEffect(() => {
+    document.title = props.title + " | PickPallet";
+    document
+      .querySelector("meta[name='description']")
+      .setAttribute("content", props.description || "");
+  }, [props]);
+
+  return <></>;
+};
+
+export default Head;
