4f0b9c63258657c1b41d86df066914d5b99e8423
Caio Augusto <caioamfr@gmail.com>
2020-11-10 14:01:52 -0300

    Dados passados por propriedades

--- src/Components/PalletDetails/PalletDetails.js
+++ src/Components/PalletDetails/PalletDetails.js
@@ -1,82 +1,39 @@
 import React, { useEffect, useState } from "react";
-import firebase from "../../config/firebase";
 import { useParams } from "react-router-dom";
-import { Wrapper, BoxPallet, PalletColors } from "./style";
+import { Wrapper } from "./style";
 import { Container } from "../../style/GlobalStyle";
-import Button from "../Button/Button";
+
 import Loading from "../Loading/Loading";
+import base_url from "../../services/api_url";
+import Pallet from "../PalletDetails/Pallet";
 
 const PalletDetails = () => {
-  const [pallet, setPallet] = useState([]);
+  const [pallet, setPallet] = useState(null);
   const [loading, setLoading] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
     setLoading(true);
-    firebase
-      .firestore()
-      .collection("pallets")
-      .doc(id)
-      .get()
-      .then((result) => {
-        setPallet(result.data());
-        setLoading(false);
-      });
+    let isMounted = true;
+    const fetchPalletDetailsById = async () => {
+      const response = await fetch(`${base_url}/${id}`);
+      const json = await response.json();
+      if (isMounted) setPallet(json);
+    };
+    fetchPalletDetailsById();
+    setLoading(false);
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   if (pallet === null) return null;
+
   return (
     <Container>
       <Wrapper>
         {loading && <Loading />}
-        <BoxPallet>
-          <PalletColors>
-            <div style={{ backgroundColor: `${pallet.color1}` }}>
-              <p
-                onClick={({ target }) =>
-                  navigator.clipboard.writeText(`${target.innerHTML}`)
-                }
-              >
-                {pallet.color1}
-              </p>
-            </div>
-            <div style={{ backgroundColor: `${pallet.color2}` }}>
-              <p
-                onClick={({ target }) =>
-                  navigator.clipboard.writeText(`${target.innerHTML}`)
-                }
-              >
-                {pallet.color2}
-              </p>
-            </div>
-            <div style={{ backgroundColor: `${pallet.color3}` }}>
-              <p
-                onClick={({ target }) =>
-                  navigator.clipboard.writeText(`${target.innerHTML}`)
-                }
-              >
-                {pallet.color3}
-              </p>
-            </div>
-            <div style={{ backgroundColor: `${pallet.color4}` }}>
-              <p
-                onClick={({ target }) =>
-                  navigator.clipboard.writeText(`${target.innerHTML}`)
-                }
-              >
-                {pallet.color4}
-              </p>
-            </div>
-          </PalletColors>
-          <Button
-            buttonText="Link"
-            onClick={({ target }) =>
-              navigator.clipboard.writeText(
-                `pickpallet.netlify.app/paleta/${id}`
-              )
-            }
-          />
-        </BoxPallet>
+        {!loading && pallet !== null && <Pallet pallet={pallet} id={id} />}
       </Wrapper>
     </Container>
   );
