4173c0a878f7ac0516758ac4d075c3408cdc49dd
Caio Augusto <caioamfr@gmail.com>
2020-11-10 09:20:21 -0300

    Listagem de paletas através da API

--- src/Components/PalletBox/PalletBox.js
+++ src/Components/PalletBox/PalletBox.js
@@ -1,87 +1,79 @@
 import React, { useEffect, useState } from "react";
-import firebase from "../../config/firebase";
 import { Wrapper, BoxPallet, PalletColors } from "./style";
 import { Container } from "../../style/GlobalStyle";
 import Button from "../Button/Button";
 import { useNavigate } from "react-router-dom";
 import Loading from "../Loading/Loading";
+import base_url from "../../services/api_url";
 
 const PalletBox = () => {
   const [pallets, setPallets] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  let allPallets = [];
 
   useEffect(() => {
     setLoading(true);
-    firebase
-      .firestore()
-      .collection("pallets")
-      .get()
-      .then(result => {
-        result.docs.forEach((doc) => {
-          allPallets.push({
-            id: doc.id,
-            ...doc.data(),
-          });     
-        });
-        setLoading(false);
-        setPallets(allPallets);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-      
+    const fetchAllPallets = async () => {
+      const response = await fetch(base_url);
+      const json = await response.json();
+      setPallets(json);
+    };
+    fetchAllPallets();
+    setLoading(false);
   }, []);
 
   if (pallets === null) return null;
-  return (   
+  return (
     <Container>
       <Wrapper>
         {loading && <Loading />}
         {pallets.map(pallet => (
           <div key={pallet.id}>
-            <BoxPallet >
+            <BoxPallet>
               <PalletColors>
-                <div style={{ backgroundColor: `${pallet.color1}` }}>
+                <div style={{ backgroundColor: `${pallet.pallet1}` }}>
                   <p
                     onClick={({ target }) =>
                       navigator.clipboard.writeText(`${target.innerHTML}`)
                     }
                   >
-                    {pallet.color1}
+                    {pallet.pallet1}
                   </p>
                 </div>
-                <div style={{ backgroundColor: `${pallet.color2}` }}>
+                <div style={{ backgroundColor: `${pallet.pallet2}` }}>
                   <p
                     onClick={({ target }) =>
                       navigator.clipboard.writeText(`${target.innerHTML}`)
                     }
                   >
-                    {pallet.color2}
+                    {pallet.pallet2}
                   </p>
                 </div>
-                <div style={{ backgroundColor: `${pallet.color3}` }}>
+                <div style={{ backgroundColor: `${pallet.pallet3}` }}>
                   <p
                     onClick={({ target }) =>
                       navigator.clipboard.writeText(`${target.innerHTML}`)
                     }
                   >
-                    {pallet.color3}
+                    {pallet.pallet3}
                   </p>
                 </div>
-                <div style={{ backgroundColor: `${pallet.color4}` }}>
+                <div style={{ backgroundColor: `${pallet.pallet4}` }}>
                   <p
                     onClick={({ target }) =>
                       navigator.clipboard.writeText(`${target.innerHTML}`)
                     }
                   >
-                    {pallet.color4}
+                    {pallet.pallet4}
                   </p>
                 </div>
               </PalletColors>
-              <Button value={pallet.id} buttonText="Detalhes" onClick={() => navigate(`/paleta/${pallet.id}`)} />
-            </BoxPallet>  
+              <Button
+                value={pallet.id}
+                buttonText="Detalhes"
+                onClick={() => navigate(`/paleta/${pallet.id}`)}
+              />
+            </BoxPallet>
           </div>
         ))}
       </Wrapper>
