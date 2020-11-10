a99edb5c0239d865704e95aea1c5ded8962fe1ae
Caio Augusto <caioamfr@gmail.com>
2020-11-10 11:56:11 -0300

    Scroll infinito

--- src/Components/PalletBox/PalletBox.js
+++ src/Components/PalletBox/PalletBox.js
@@ -9,25 +9,53 @@ import base_url from "../../services/api_url";
 const PalletBox = () => {
   const [pallets, setPallets] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [limit, setLimit] = useState(18)
+  const [infinite, setInfinite] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
+    const total = 18;
     setLoading(true);
     const fetchAllPallets = async () => {
-      const response = await fetch(base_url);
+      const response = await fetch(`${base_url}?_limit=${limit}`);
       const json = await response.json();
+      if (response.ok && json.length < total) setInfinite(false);
       setPallets(json);
     };
     fetchAllPallets();
     setLoading(false);
-  }, []);
+  }, [infinite, limit]);
+
+  useEffect(() => {
+    let wait = false;
+    const infiniteScroll = () => {
+      if (infinite) {
+        const pageScroll = window.scrollY;
+        const pageHeight = document.body.offsetHeight - window.innerHeight;
+        if (pageScroll > pageHeight * .75 && !wait) {
+          setLimit((limit) => limit * 2)
+          wait = true;
+          setTimeout(() => {
+            wait = false;
+          }, 500)
+        }
+      }
+    }
+
+    window.addEventListener('wheel', infiniteScroll)
+    window.addEventListener('scroll', infiniteScroll)
+    return () => {
+      window.removeEventListener('wheel', infiniteScroll)
+      window.removeEventListener('scroll', infiniteScroll)
+    }
+  })
 
   if (pallets === null) return null;
   return (
     <Container>
       <Wrapper>
         {loading && <Loading />}
-        {pallets.map(pallet => (
+        {pallets.length !== undefined && pallets.map(pallet => (
           <div key={pallet.id}>
             <BoxPallet>
               <PalletColors>
@@ -77,7 +105,7 @@ const PalletBox = () => {
           </div>
         ))}
       </Wrapper>
-    </Container>
+    </Container >
   );
 };
 
