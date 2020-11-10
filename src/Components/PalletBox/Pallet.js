9fde21d9746cc4a5531868ad3db8a4de098714e1
Caio Augusto <caioamfr@gmail.com>
2020-11-10 13:45:54 -0300

    Componente recebendo dados através de props

---
+++ src/Components/PalletBox/Pallet.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../Button/Button";
+import { BoxPallet, PalletColors } from "./style";
+
+const Pallet = ({ pallets }) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      {pallets.length !== undefined &&
+        pallets.map((pallet, index) => (
+          <div key={pallet.id}>
+            <BoxPallet style={{ animationDelay: `${index * 5}ms` }}>
+              <PalletColors>
+                <div style={{ backgroundColor: `${pallet.pallet1}` }}>
+                  <p
+                    onClick={({ target }) =>
+                      navigator.clipboard.writeText(`${target.innerHTML}`)
+                    }
+                  >
+                    {pallet.pallet1}
+                  </p>
+                </div>
+                <div style={{ backgroundColor: `${pallet.pallet2}` }}>
+                  <p
+                    onClick={({ target }) =>
+                      navigator.clipboard.writeText(`${target.innerHTML}`)
+                    }
+                  >
+                    {pallet.pallet2}
+                  </p>
+                </div>
+                <div style={{ backgroundColor: `${pallet.pallet3}` }}>
+                  <p
+                    onClick={({ target }) =>
+                      navigator.clipboard.writeText(`${target.innerHTML}`)
+                    }
+                  >
+                    {pallet.pallet3}
+                  </p>
+                </div>
+                <div style={{ backgroundColor: `${pallet.pallet4}` }}>
+                  <p
+                    onClick={({ target }) =>
+                      navigator.clipboard.writeText(`${target.innerHTML}`)
+                    }
+                  >
+                    {pallet.pallet4}
+                  </p>
+                </div>
+              </PalletColors>
+              <Button
+                value={pallet.id}
+                buttonText="Detalhes"
+                onClick={() => navigate(`/paleta/${pallet.id}`)}
+              />
+            </BoxPallet>
+          </div>
+        ))}
+    </>
+  );
+};
+
+export default Pallet;
