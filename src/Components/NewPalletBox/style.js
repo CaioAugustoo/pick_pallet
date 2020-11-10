a42b7cb8f77130eb952b888a4b7c335b54119b50
Caio Augusto <caioamfr@gmail.com>
2020-11-10 17:35:59 -0300

    Adicionada animações

--- src/Components/NewPalletBox/style.js
+++ src/Components/NewPalletBox/style.js
@@ -1,16 +1,30 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const item = keyframes`
+  from {
+    opacity: 0
+    transform: translateY(0.625rem);
+  } to {
+    opacity: 1;
+    transform: translateY(0rem)
+  }
+`;
 
 export const Wrapper = styled.div`
   display: flex;
   margin-top: 7.5rem;
   align-items: center;
   flex-direction: column;
+  animation: ${item} 0.4s ease;
+  animation-fill-mode: both;
+  opacity: 0;
+  transform: translateY(0.625rem);
 
   h3 {
     margin-bottom: 1.875rem;
     font-weight: 400;
   }
-  
+
   button {
     margin: 1.875rem auto;
   }
