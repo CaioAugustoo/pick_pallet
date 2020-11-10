c149fcf71d30774fdae71e63b08f81861208ce91
Caio Augusto <caioamfr@gmail.com>
2020-11-10 16:08:53 -0300

    Componente Head adicionado

--- src/pages/Create/Create.js
+++ src/pages/Create/Create.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../Components/Header/Header';
 import NewPalletBox from '../../Components/NewPalletBox/NewPalletBox';
+import Head from "../../Components/Helper/Head/Head";
 
 const Create = () => {
   document.title = 'Pick Pallet | Criar paleta';
   return (
     <div>
+      <Head title="Criar paleta" />
       <Header />
       <NewPalletBox />
     </div>
