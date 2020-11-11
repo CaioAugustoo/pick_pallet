7d0df243e820af6566474d4e81831e8f265807ab
Caio Augusto <caioamfr@gmail.com>
2020-11-10 21:49:37 -0300

    Adição do toastify

--- src/Components/PalletBox/PalletBox.js
+++ src/Components/PalletBox/PalletBox.js
@@ -1,66 +1,75 @@
-import React, { useEffect, useState } from "react";
-import { Wrapper } from "./style";
-import { Container } from "../../style/GlobalStyle";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../Button/Button";
+import Toastify from "../Toastify/Toastify";
+import { BoxPallet, PalletColors, Info, Wrap } from "./style";
 
-import Loading from "../Loading/Loading";
-import base_url from "../../services/api_url";
-import Pallet from "./Pallet";
+const Pallet = ({ pallets }) => {
+  const navigate = useNavigate();
+  const [isToastyfied, setIsToastifyied] = useState(false);
 
-const PalletBox = () => {
-  const [pallets, setPallets] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [limit, setLimit] = useState(18);
-  const [infinite, setInfinite] = useState(true);
+  const copyToClipBoard = target => {
+    navigator.clipboard.writeText(`${target}`);
+    setIsToastifyied(true);
+  };
 
-  useEffect(() => {
-    setLoading(true);
-    const total = 18;
-    const fetchAllPallets = async () => {
-      const response = await fetch(
-        `${base_url}?_limit=${limit}&_sort=published_at%3ADESC`
-      );
-      const json = await response.json();
-      if (response.ok && json.length < total) setInfinite(false);
-      setPallets(json);
-    };
-    fetchAllPallets();
-    setLoading(false);
-  }, [infinite, limit]);
-
-  useEffect(() => {
-    let wait = false;
-
-    const infiniteScroll = () => {
-      if (infinite) {
-        const scroll = window.scrollY;
-        const height = document.body.offsetHeight - window.innerHeight;
-        if (scroll > height * 0.85 && !wait) {
-          setLimit(limit => limit + 18);
-          wait = true;
-          setTimeout(() => {
-            wait = false;
-          }, 500);
-        }
-      }
-    };
-
-    window.addEventListener("wheel", infiniteScroll);
-    window.addEventListener("scroll", infiniteScroll);
-    return () => {
-      window.removeEventListener("wheel", infiniteScroll);
-      window.removeEventListener("scroll", infiniteScroll);
-    };
-  }, [infinite, pallets]);
-
-  if (pallets === null) return null;
   return (
-    <Container>
-      <Wrapper>
-        {loading && <Loading />}
-        {!loading && pallets !== null && <Pallet pallets={pallets} />}
-      </Wrapper>
-    </Container>
+    <>
+      {pallets.length !== undefined &&
+        pallets.map((pallet, index) => (
+          <div key={pallet.id}>
+            <BoxPallet style={{ animationDelay: `${index * 5}ms` }}>
+              <PalletColors>
+                <div style={{ backgroundColor: `${pallet.pallet1}` }}>
+                  <p
+                    onClick={({ target }) => copyToClipBoard(target.innerHTML)}
+                  >
+                    {pallet.pallet1}
+                  </p>
+                </div>
+                <div style={{ backgroundColor: `${pallet.pallet2}` }}>
+                  <p
+                    onClick={({ target }) => copyToClipBoard(target.innerHTML)}
+                  >
+                    {pallet.pallet2}
+                  </p>
+                </div>
+                <div style={{ backgroundColor: `${pallet.pallet3}` }}>
+                  <p
+                    onClick={({ target }) => copyToClipBoard(target.innerHTML)}
+                  >
+                    {pallet.pallet3}
+                  </p>
+                </div>
+                <div style={{ backgroundColor: `${pallet.pallet4}` }}>
+                  <p
+                    onClick={({ target }) => copyToClipBoard(target.innerHTML)}
+                  >
+                    {pallet.pallet4}
+                  </p>
+                </div>
+              </PalletColors>
+              <Wrap>
+                <Button
+                  value={pallet.id}
+                  buttonText="Detalhes"
+                  onClick={() => navigate(`/paleta/${pallet.id}`)}
+                />
+                <Info className="pallet__date">
+                  {new Date(pallet.createdAt).toLocaleDateString()}
+                </Info>
+              </Wrap>
+            </BoxPallet>
+          </div>
+        ))}
+
+      <Toastify
+        isToastyfied={isToastyfied}
+        setIsToastifyied={setIsToastifyied}
+        toastifyText="😎 Copiado!"
+      />
+    </>
   );
 };
 
-export default PalletBox;
+export default Pallet;
