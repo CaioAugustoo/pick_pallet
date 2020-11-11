29cf96cf5072eac32c3d2b2651ee99b0ac91c9d4
Caio Augusto <caioamfr@gmail.com>
2020-11-10 22:02:46 -0300

    Uso do Toastify

--- src/Components/PalletDetails/Pallet.js
+++ src/Components/PalletDetails/Pallet.js
@@ -1,44 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { BoxPallet, PalletColors, Info, Wrap } from "./style";
 import Button from "../Button/Button";
+import Toastify from "../Toastify/Toastify";
 
 const Pallet = ({ pallet, id }) => {
+  const [isToastyfied, setIsToastifyied] = useState(false);
+
+  const copyToClipBoard = target => {
+    navigator.clipboard.writeText(`${target}`);
+    setIsToastifyied(true);
+  };
+
+  const copyLinkToClipBoard = () => {
+    navigator.clipboard.writeText(`pickpallet.netlify.app/paleta/${id}`);
+    setIsToastifyied(true);
+  };
+
   return (
     <BoxPallet>
       <PalletColors>
         <div style={{ backgroundColor: `${pallet.pallet1}` }}>
-          <p
-            onClick={({ target }) =>
-              navigator.clipboard.writeText(`${target.innerHTML}`)
-            }
-          >
+          <p onClick={({ target }) => copyToClipBoard(target.innerHTML)}>
             {pallet.pallet1}
           </p>
         </div>
         <div style={{ backgroundColor: `${pallet.pallet2}` }}>
-          <p
-            onClick={({ target }) =>
-              navigator.clipboard.writeText(`${target.innerHTML}`)
-            }
-          >
+          <p onClick={({ target }) => copyToClipBoard(target.innerHTML)}>
             {pallet.pallet2}
           </p>
         </div>
         <div style={{ backgroundColor: `${pallet.pallet3}` }}>
-          <p
-            onClick={({ target }) =>
-              navigator.clipboard.writeText(`${target.innerHTML}`)
-            }
-          >
+          <p onClick={({ target }) => copyToClipBoard(target.innerHTML)}>
             {pallet.pallet3}
           </p>
         </div>
         <div style={{ backgroundColor: `${pallet.pallet4}` }}>
-          <p
-            onClick={({ target }) =>
-              navigator.clipboard.writeText(`${target.innerHTML}`)
-            }
-          >
+          <p onClick={({ target }) => copyToClipBoard(target.innerHTML)}>
             {pallet.pallet4}
           </p>
         </div>
@@ -46,12 +43,15 @@ const Pallet = ({ pallet, id }) => {
       <Wrap>
         <Button
           buttonText="Link"
-          onClick={() =>
-            navigator.clipboard.writeText(`pickpallet.netlify.app/paleta/${id}`)
-          }
+          onClick={({ target }) => copyLinkToClipBoard()}
         />
         <Info>{new Date(pallet.createdAt).toLocaleDateString()}</Info>
       </Wrap>
+      <Toastify
+        isToastyfied={isToastyfied}
+        setIsToastifyied={setIsToastifyied}
+        toastifyText="😎 Copiado!"
+      />
     </BoxPallet>
   );
 };
