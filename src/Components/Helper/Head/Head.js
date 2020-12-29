7d11c72be426933f5b10240089eecb179444eb7d
Caio Augusto <caioamfr@gmail.com>
2020-12-29 19:54:57 -0300

    Remoção da description vazia

--- src/Components/Helper/Head/Head.js
+++ src/Components/Helper/Head/Head.js
@@ -3,9 +3,6 @@ import React, { useEffect } from "react";
 const Head = props => {
   useEffect(() => {
     document.title = props.title + " | PickPallet";
-    document
-      .querySelector("meta[name='description']")
-      .setAttribute("content", props.description || "");
   }, [props]);
 
   return <></>;
