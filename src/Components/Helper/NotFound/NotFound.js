9a6e8d611a30e42e3d5096f49ed54186cc39cf3d
caioaugustoo <caioamfr@gmail.com>
2021-01-25 06:13:18 -0300

    Criado componente de not found

---
+++ src/Components/Helper/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Wrapper, Title, SubTitle } from "./styles";
+
+const NotFound404 = () => {
+  return (
+    <Wrapper>
+      <Title>Ooppss...</Title>
+      <SubTitle>A paleta pela qual você procura ainda não foi criada.</SubTitle>
+    </Wrapper>
+  );
+};
+
+export default NotFound404;
---
+++ src/Components/Helper/NotFound/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.875rem;
+`;
+
+export const SubTitle = styled.p`
+  font-size: 1.125rem;
+  margin-top: 10px;
+  text-align: center;
+`;
