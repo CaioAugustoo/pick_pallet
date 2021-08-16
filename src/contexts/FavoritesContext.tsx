ea94a8cbef9e417f99eeb2a459a0abfce659dd3e
caioaugustoo <caioamfr@gmail.com>
2021-08-15 21:35:12 -0300

    Criada função para remover como favorita

--- src/contexts/FavoritesContext.tsx
+++ src/contexts/FavoritesContext.tsx
@@ -5,6 +5,8 @@ import { IPallet } from "components/Pallet";
 interface IContext {
   favorites: IPallet[];
   saveAsFavorite: (pallet: IPallet) => void;
+  isFavorited: (palletId: number) => boolean;
+  removeAsFavorite: (palletId: number) => void;
 }
 
 interface IFavorites {
@@ -25,19 +27,42 @@ export const Favorites = ({ children }: IFavorites) => {
     }
   });
 
+  const isFavorited = useCallback(
+    (palletId: number): boolean => {
+      const isFavorited = favorites?.some(el => el.id === palletId);
+
+      return isFavorited;
+    },
+    [favorites]
+  );
+
   const saveAsFavorite = useCallback(
     (pallet: IPallet) => {
+      const alreadyFavorited = isFavorited(pallet.id);
+      if (alreadyFavorited) return;
+
       setFavorites(prev => [...prev, pallet]);
     },
     [favorites]
   );
+
+  const removeAsFavorite = useCallback(
+    (palletId: number) => {
+      setFavorites(prev => prev.filter(el => el.id !== palletId));
+    },
+    [favorites]
+  );
+
+  console.log(favorites);
 
   useEffect(() => {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(favorites));
   }, [favorites]);
 
   return (
-    <FavoritesContext.Provider value={{ favorites, saveAsFavorite }}>
+    <FavoritesContext.Provider
+      value={{ favorites, saveAsFavorite, isFavorited, removeAsFavorite }}
+    >
       {children}
     </FavoritesContext.Provider>
   );
