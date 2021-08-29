0b861dab12992164c3a69cf97a6e1e512b913817
caioaugustoo <caioamfr@gmail.com>
2021-08-29 13:56:17 -0300

    Estilos dos toasts

--- src/utils/toasts/index.ts
+++ src/utils/toasts/index.ts
@@ -1,17 +1,21 @@
 import toast from "react-hot-toast";
 
-const toastIcon = {
+const styles = {
+  style: {
+    background: "var(--toast-bg)",
+    color: "var(--toast-text)",
+  },
   icon: "🎨",
 };
 
 export function toastColor() {
-  toast.success("Cor copiada!", toastIcon);
+  toast.success("Cor copiada!", styles);
 }
 
 export function toastFavorited() {
-  toast.success("Marcada como favorita!", toastIcon);
+  toast.success("Marcada como favorita!", styles);
 }
 
 export function toastRemovedFavorite() {
-  toast.success("Removida como favorita!", toastIcon);
+  toast.success("Removida como favorita!", styles);
 }
